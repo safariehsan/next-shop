368828e0d40d7e490a5ebd1622f9c447c968e990
safariehsan <ehsansafari@hotmail.com>
2026-02-06 21:56:56 +0330

    install stripe + add some demo products

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import { stripe } from "@/lib/stripe";
+
+export default async function Home() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 4,
+  });
+  console.log(products);
   return (
     <main>
       <h1 className="font-bold">Home Page</h1>
