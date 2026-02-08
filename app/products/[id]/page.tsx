8ee4412f89fb86aeffa33b8769d3194f7887f3fc
safariehsan <ehsansafari@hotmail.com>
2026-02-08 17:14:00 +0330

    hotfix: resolve id params [id]

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import { stripe } from "@/lib/stripe";
 import ProductDetails from "@/components/ProductDetails";
 
-const ProductPage = async ({ params }: { params: { id: string } }) => {
-  const currentProduct = await stripe.products.retrieve(params.id, {
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+const ProductPage = async (props: PageProps) => {
+  const params = await props.params;
+  const { id } = params;
+  const currentProduct = await stripe.products.retrieve(id, {
     expand: ["default_price"],
   });
   const plainProduct = JSON.parse(JSON.stringify(currentProduct));
