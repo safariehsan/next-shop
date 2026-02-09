ed311bfd7be510c50415dd9cdbfccf3311fcc58a
safariehsan <ehsansafari@hotmail.com>
2026-02-09 18:23:32 +0330

    hotfix:add try-catch to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,27 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
-  const products = await stripe.products.list({
-    expand: ["data.default_price"],
-    limit: 4,
-  });
-  return (
-    <main>
-      <h1 className="font-bold">Welcome to the NextShop!</h1>
-      <Link href="/products">Browse All Products</Link>
-      <Image
-        src={products.data[0].images[0]}
-        alt={products.data[0].name}
-        width={300}
-        height={300}
-        unoptimized
-      />
-      <Carousel products={products.data} />
-    </main>
-  );
+  try {
+    const products = await stripe.products.list({
+      expand: ["data.default_price"],
+      limit: 4,
+    });
+    return (
+      <main>
+        <h1 className="font-bold">Welcome to the NextShop!</h1>
+        <Link href="/products">Browse All Products</Link>
+        <Image
+          src={products.data[0].images[0]}
+          alt={products.data[0].name}
+          width={300}
+          height={300}
+          unoptimized
+        />
+        <Carousel products={products.data} />
+      </main>
+    );
+  } catch (err) {
+    console.log(err);
+    return <p>unable to load the products...</p>;
+  }
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,15 +3,24 @@ import ProductsList from "@/components/ProductsList";
 import { stripe } from "@/lib/stripe";
 
 const ProductsPage = async () => {
-  const products = await stripe.products.list({
-    expand: ["data.default_price"],
-  });
-  return (
-    <div>
-      <h1>All Products Of NextShop</h1>
-      <ProductsList products={products.data} />
-    </div>
-  );
+  try {
+    const products = await stripe.products.list({
+      expand: ["data.default_price"],
+    });
+    return (
+      <div>
+        <h1>All Products Of NextShop</h1>
+        <ProductsList products={products.data} />
+      </div>
+    );
+  } catch (err) {
+    console.log(err);
+    return (
+      <div>
+        <p> Unable to load products at the moment. Please try again later.</p>
+      </div>
+    );
+  }
 };
 
 export default ProductsPage;
